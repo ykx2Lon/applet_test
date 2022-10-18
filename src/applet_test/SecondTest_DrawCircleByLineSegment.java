b8b59e674882b8c706764f079952eddf53ff8c7c
ykx2Lon <ykx2Lon@gmail.com>
2022-10-18 22:47:25 +0800

    用直線線段畫圓

--- src/applet_test/SecondTest_DrawCircleByLineSegment.java
+++ src/applet_test/SecondTest_DrawCircleByLineSegment.java
@@ -23,8 +23,9 @@ public class SecondTest_DrawCircleByLineSegment extends Applet {
 		paintGrid(g);
 		
 		//隨便畫一些圖形
-		paintSomeGeometry(g);
-		
+//		paintSomeGeometry(g);
+		//畫出圓
+		drawCircle(g,100,100,30,50,Color.BLUE);
 		
 		super.paint(g);
 	}
@@ -59,6 +60,75 @@ public class SecondTest_DrawCircleByLineSegment extends Applet {
 		int [] xArray = {430,410,430,410,430,410} ;
 		int [] yArray = {10,50,60,100,110,150};
 		g.drawPolygon( xArray, yArray, xArray.length);
+		g.setColor(originalColor);
+	}
+	
+	//畫出定位點
+	private void drawDot(Graphics g,int x, int y,int r,Color color) {
+		Color originalColor = g.getColor();
+		g.setColor(color);
+		g.fillOval(x-r, y-r,r*2 , r*2);
+		g.setColor(originalColor);
+	}
+	
+	//畫出輔助弧線，用來表示角度
+		private void drawAngle(Graphics g,int x, int y,int r,int degree,Color color) {
+			Color originalColor = g.getColor();
+			g.setColor(color);
+			g.drawArc(x-r, y-r,r*2 , r*2, 0,degree);
+			g.setColor(originalColor);
+		}
+	
+	
+	/***畫出圓
+	 * 
+	 * @param g 
+	 * @param x 圓心x
+	 * @param y 圓心y
+	 * @param r 半徑
+	 * @param accuracy 精度，表示用幾條線段構成這個圓，值越高越像個圓
+	 * @param color
+	 */
+	private void drawCircle(Graphics g,int x, int y,int r,int accuracy,Color color) {
+		Color originalColor = g.getColor();
+		
+		//標出圓心
+		drawDot(g,x,y,3,Color.red);
+		//每線段對應角度
+		float perDegree = 360/(float)accuracy;
+		//當前xy，畫出線段用
+		double nowx=0,nowy=0;
+		//上一個點的xy，畫出線段用
+		double lastx=0,lasty=0;
+		for(int i =0;i<=accuracy;i++) {
+			//當前角度
+			float nowDegree = perDegree*i;
+			System.out.println("nowDegree:"+nowDegree);
+			//x = 半徑*cos(theda)+圓心，y=半徑*sin(theda)+圓心
+			//pi = 圓周/直徑，
+			nowx = r*Math.cos(nowDegree/180*Math.PI)+x;
+			nowy = r*Math.sin(nowDegree/180*Math.PI)+y;
+			//每個點顏色越來越淡
+			Color nowColor = new Color(1*i,255/accuracy*i,255/accuracy*i);
+			drawDot(g,(int)Math.round(nowx),(int)Math.round(nowy), 3, nowColor);
+			
+			//角度輔助線
+//			g.setColor(nowColor);
+//			drawAngle(g,x,y,i*r/accuracy,-(int)nowDegree,nowColor);
+//			g.drawLine(x, y,(int)Math.round(nowx),(int)Math.round(nowy));
+//			g.drawLine((int)Math.round(nowx), y,(int)Math.round(nowx),(int)Math.round(nowy));
+//			g.setColor(Color.black);
+			
+			//第一個點以外皆和上一個點連線畫出線段
+			
+			if(i>0) {
+				g.drawLine((int)Math.round(lastx), (int)Math.round(lasty),(int)Math.round(nowx),(int)Math.round(nowy));
+			}
+			lastx = nowx;
+			lasty = nowy;
+		}
+		g.setColor(color);
+		
 		g.setColor(originalColor);
 	}
 
